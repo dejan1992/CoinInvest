b40d0e499f7d592ec7ce1c4d9b4967d858d38b59
Dejan Cetojevic <dejan92ceta@live.com>
2021-09-29 15:05:42 +0200

    code comments added

--- swipe-table-message.js
+++ swipe-table-message.js
@@ -1,6 +1,7 @@
 const info = document.getElementById("info");
 let windowWidth = window.innerWidth;
 
+// Check window width
 if (windowWidth <= 1000) {
   const swipeMessage = document.createElement("p");
   swipeMessage.innerHTML = '<i class="far fa-hand-pointer"></i> Swipe table horizontally for more data';
